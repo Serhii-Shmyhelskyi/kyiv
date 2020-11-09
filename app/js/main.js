465fa60c0b1f46a0612869fae8ffdb20d65f45c8
ShmyhelskyiSerhii <siro181a@gmail.com>
2020-11-09 11:39:08 +0200

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+$(function () {
+
+    $('.photo__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').slideToggle();
+    });
+
+    $('.footer__item-title--company').on('click', function () {
+        $('.footer__item-list--company').slideToggle();
+    });
+
+    $('.footer__item-title--detal').on('click', function () {
+        $('.footer__item-list--detal').slideToggle();
+    });
+
+    $('.footer__item-title--contacts').on('click', function () {
+        $('.footer__item-list--contacts').slideToggle();
+    });
+
+    $('.footer__item-title--blog').on('click', function () {
+        $('.footer__item-list--blog').slideToggle();
+    });
+})
